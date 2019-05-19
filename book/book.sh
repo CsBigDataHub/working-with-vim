ebf8b026998f383f1b627a9bcc0164e85354462b
Marcus Kazmierczak <marcus@mkaz.com>
2019-05-18 17:53:28 -0700

    Script to generate epub, easier for proof reading

---
+++ book/book.sh
@@ -0,0 +1,24 @@
+pandoc -o wwv.epub title.txt \
+    ../pages/intro.md \
+    ../pages/modes.md \
+    ../pages/help.md \
+    ../pages/tao.md \
+    ../pages/motions.md \
+    ../pages/operators.md \
+    ../pages/lines.md \
+    ../pages/text-object.md \
+    ../pages/search.md \
+    ../pages/nav-marks.md \
+    ../pages/substitute.md \
+    ../pages/read.md \
+    ../pages/registers.md \
+    ../pages/buffers.md \
+    ../pages/windows.md \
+    ../pages/recording.md \
+    ../pages/configuration.md \
+    ../pages/plugins.md \
+    ../pages/sessions.md \
+    ../pages/snippets.md \
+    ../pages/terminal.md \
+    ../pages/writing.md \
+    ../pages/vimwiki.md
